d82eca8be2c7c3e01c61db323844eefcbdb185b9
duner <asduner@gmail.com>
2014-09-17 14:17:56 -0400

    Add is_published to models, change status options

--- elections/posts/models.py
+++ elections/posts/models.py
@@ -1,12 +1,5 @@
 from django.db import models
 
-STATUS_CHOICES = (
-    ('d', 'Draft'),
-    ('p', 'Read Me'),
-    ('p', 'Edited'),
-    ('p', 'Published'),
-)
-
 class Author(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
@@ -14,12 +7,25 @@ class Author(models.Model):
     	return self.name
 
 class Post(models.Model):
+    DRAFT = 'd'
+    README = 'r'
+    EDITED = 'e'
+    PUBLISHED = p
+    STATUS_CHOICES = (
+        (DRAFT, 'Draft'),
+        (README, 'Read Me'),
+        (EDITED, 'Edited'),
+        (PUBLISHED, 'Published'),
+    )
     title = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
     body = models.TextField()
     posted_datetime = models.DateTimeField(auto_now_add=True, db_index=True)
     author = models.ManyToManyField('Author', null=True, blank=True)
-    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='d')
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default=DRAFT)
 
     def __unicode__(self):
     	return self.title
+
+    def is_published(self):
+        return self.status == self.PUBLISHED
