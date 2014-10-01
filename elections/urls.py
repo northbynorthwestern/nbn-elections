d620d56b5026e704bc33d9cead92103a0fc89bca
duner <asduner@gmail.com>
2014-09-30 21:53:43 -0500

    Fix URLs

--- elections/urls.py
+++ elections/urls.py
@@ -11,7 +11,7 @@ urlpatterns = patterns('',
     (r'^tinymce/', include('tinymce.urls')),
 
     url(r'^2014', views.LandingView.as_view(), name='landing'),
-    url(r'^2014/post/(?P<slug>[\w-]+)/$', views.PostView.as_view(), name='post'),
+    url(r'^post/(?P<slug>[\w-]+)/$', views.PostView.as_view(), name='post'),
     url(r'^proxy/(?P<race>.*)', views.ProxyView.as_view(), name='proxy'),
 
     url(r'^admin/', include(admin.site.urls)),
