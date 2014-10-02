d5019bd1e6772ab94c41ed0d39588213726bb730
duner <asduner@gmail.com>
2014-10-01 20:08:34 -0500

    Make secret key secret

--- elections/config/settings/production.py
+++ elections/config/settings/production.py
@@ -16,7 +16,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__fil
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'uc8t9r!kjkqj$zqml77k=&jn=q3gh@@_4^6durlfu(xy!ro3m&'
+SECRET_KEY = os.environ.get('ELECTIONS_DJANGO_SECRET_KEY', None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
