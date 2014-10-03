1a99a07cc6c00fbd291258d15d2af8364b2c81c8
jeremyjbowers <jeremyjbowers@gmail.com>
2014-10-02 21:15:53 -0400

    Fix urls for admin.

--- elections/urls.py
+++ elections/urls.py
@@ -9,12 +9,10 @@ urlpatterns = patterns('',
     # url(r'^$', 'elections.views.home', name='home'),
     # url(r'^posts/', include('posts.urls')),
 
-    (r'^elections/tinymce/', include('tinymce.urls')),
-
-    url(r'^elections/2014/post/(?P<slug>[\w-]+)/$', views.PostView.as_view(), name='post'),
-    url(r'^elections/2014', views.LandingView.as_view(), name='landing'),
-    url(r'^elections/', RedirectView.as_view(url='/elections/2014/'), name='redirect-to-landing'),
-    url(r'^elections/proxy/(?P<race>.*)', views.ProxyView.as_view(), name='proxy'),
-
+    url(r'^elections/tinymce/', include('tinymce.urls')),
     url(r'^elections/admin/', include(admin.site.urls)),
+    url(r'^elections/2014/post/(?P<slug>[\w-]+)/$', views.PostView.as_view(), name='post'),
+    url(r'^elections/2014/', views.LandingView.as_view(), name='landing'),
+    url(r'^elections/proxy/(?P<race>.*)/', views.ProxyView.as_view(), name='proxy'),
+    url(r'^elections/', RedirectView.as_view(url='/elections/2014/'), name='redirect-to-landing'),
 )
