ff569dc4a9946e09a3c8851cf9d070db54a65c32
jeremyjbowers <jeremyjbowers@gmail.com>
2014-10-02 20:24:14 -0400

    Namespace with elections.

--- elections/urls.py
+++ elections/urls.py
@@ -8,11 +8,11 @@ urlpatterns = patterns('',
     # url(r'^$', 'elections.views.home', name='home'),
     # url(r'^posts/', include('posts.urls')),
 
-    (r'^tinymce/', include('tinymce.urls')),
+    (r'^elections/tinymce/', include('tinymce.urls')),
 
-    url(r'^2014', views.LandingView.as_view(), name='landing'),
-    url(r'^post/(?P<slug>[\w-]+)/$', views.PostView.as_view(), name='post'),
-    url(r'^proxy/(?P<race>.*)', views.ProxyView.as_view(), name='proxy'),
+    url(r'^elections/2014', views.LandingView.as_view(), name='landing'),
+    url(r'^elections/post/(?P<slug>[\w-]+)/$', views.PostView.as_view(), name='post'),
+    url(r'^elections/proxy/(?P<race>.*)', views.ProxyView.as_view(), name='proxy'),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^elections/admin/', include(admin.site.urls)),
 )
