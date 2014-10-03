f76da3a328940fa28c683c1b9ff73803f7826d23
duner <asduner@gmail.com>
2014-10-02 20:06:58 -0500

    Urls for 2014 should work now

--- elections/urls.py
+++ elections/urls.py
@@ -10,8 +10,8 @@ urlpatterns = patterns('',
 
     (r'^tinymce/', include('tinymce.urls')),
 
+    url(r'^2014/post/(?P<slug>[\w-]+)/$', views.PostView.as_view(), name='post'),
     url(r'^2014', views.LandingView.as_view(), name='landing'),
-    url(r'^post/(?P<slug>[\w-]+)/$', views.PostView.as_view(), name='post'),
     url(r'^proxy/(?P<race>.*)', views.ProxyView.as_view(), name='proxy'),
 
     url(r'^admin/', include(admin.site.urls)),
